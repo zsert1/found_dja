e30d3f0a1ac96a2ac86205d28fea68617f53bcd9
zsert1 <zsert1@naver.com>
2023-11-29 09:31:18 -0500

    장고 이용한 기본 로그인 만들기

--- askcompany/accounts/urls.py
+++ askcompany/accounts/urls.py
@@ -1,6 +1,9 @@
 
 from django.urls import path, include
 from .import views
+from django.contrib.auth.views import LoginView
 urlpatterns = [
+    path('login', LoginView.as_view(
+        template_name='accounts/login_form.html'), name='login'),
 
 ]
